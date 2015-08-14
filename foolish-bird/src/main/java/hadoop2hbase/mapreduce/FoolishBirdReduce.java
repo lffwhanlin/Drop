2712055d5896b1e44a9836c33968878faff32a9e
hanlin <lin.han@baifendian.com>
2015-08-14 10:47:18 +0800

    修改通过ObjectWritable在map函数和reduce函数之间传递对象报IOException

--- foolish-bird/src/main/java/hadoop2hbase/mapreduce/FoolishBirdReduce.java
+++ foolish-bird/src/main/java/hadoop2hbase/mapreduce/FoolishBirdReduce.java
@@ -2,15 +2,23 @@ package hadoop2hbase.mapreduce;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.ObjectWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class FoolishBirdReduce extends Reducer<Text, ObjectWritable, Text, ObjectWritable> {
+	
+	private byte[] family = Bytes.toBytes("person");
+	private byte[] family_name = Bytes.toBytes("name");
+	private byte[] family_mark = Bytes.toBytes("mark");
+	private byte[] family_age = Bytes.toBytes("age");
 	
 	@Override
 	protected void reduce(Text key, Iterable<ObjectWritable> value,
@@ -20,7 +28,14 @@ public class FoolishBirdReduce extends Reducer<Text, ObjectWritable, Text, Objec
 		Iterator<ObjectWritable> iterator = value.iterator();
 		while(iterator.hasNext()){
 			ObjectWritable objectWritable = iterator.next();
-			Put put =  (Put)objectWritable.get();
+			String[] columns = (String[])objectWritable.get();
+			String name = columns[0];
+			String mark = columns[1];
+			String age = columns[2];
+			Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
+			put.addColumn(family, family_name, new Date().getTime(), Bytes.toBytes(name));
+			put.addColumn(family, family_mark, new Date().getTime(), Bytes.toBytes(mark));
+			put.addColumn(family, family_age, new Date().getTime(), Bytes.toBytes(age));
 			puts.add(put);
 		}
 		HBaseUtil.insert(puts, "hadoop2hbase");
